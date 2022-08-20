2362ef05a53551ed85da444a37ff411bccdd8538
sjain20 <shubham.jain20294@gmail.com>
2022-08-20 20:53:35 +0530

    Course Project initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecepiesComponent } from './recepies/recepies.component';
+import { RecepieListComponent } from './recepies/recepie-list/recepie-list.component';
+import { RecepieDetailComponent } from './recepies/recepie-detail/recepie-detail.component';
+import { RecepieItemComponent } from './recepies/recepie-list/recepie-item/recepie-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecepiesComponent,
+    RecepieListComponent,
+    RecepieDetailComponent,
+    RecepieItemComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent
   ],
   imports: [
     BrowserModule,
